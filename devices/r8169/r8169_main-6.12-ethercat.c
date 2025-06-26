1be2bb6f2708509281eba5e6a013eebd19113525
Florian Pose <fp@igh.de>
2025-06-26 17:17:34 +0200

    Do not stop queue.

--- devices/r8169/r8169_main-6.12-ethercat.c
+++ devices/r8169/r8169_main-6.12-ethercat.c
@@ -4490,7 +4490,7 @@ static netdev_tx_t rtl8169_start_xmit(struct sk_buff *skb,
 
 	WRITE_ONCE(tp->cur_tx, tp->cur_tx + frags + 1);
 
-	stop_queue = !netif_subqueue_maybe_stop(dev, 0, rtl_tx_slots_avail(tp),
+	stop_queue = !get_ecdev(tp) && !netif_subqueue_maybe_stop(dev, 0, rtl_tx_slots_avail(tp),
 						R8169_TX_STOP_THRS,
 						R8169_TX_START_THRS);
 	if (door_bell || stop_queue)
